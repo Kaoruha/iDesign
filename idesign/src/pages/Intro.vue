ec918e0f341a4635037ae0cc56fee82a5272fb94
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2020-03-23 10:05:47 +0800

    md components

--- idesign/src/pages/Intro.vue
+++ idesign/src/pages/Intro.vue
@@ -4,12 +4,37 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
     >
-    this is intro Page
+    this is intro Page2
+    <!-- <VueMarkdown :source="htmlMD"></VueMarkdown> -->
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'IntroPage'
+  name: 'IntroPage',
+  components:{
+    // VueMarkdown
+  },
+  data() {
+      return {
+          htmlMD:{}
+      }
+  },
+  props:{
+
+  },
+  created(){
+    this.test();
+//     const url = `~spec/test.md`;
+// 　　axios.get(url).then((response) => {
+//    　　this.htmlMD = response.data;
+// 　　});
+//     print(this.htmlMD)
+  },
+  methods:{
+    test(){
+      alert(111)
+    }
+  }
 }
 </script>
---
+++ idesign/src/spec/test.md
@@ -0,0 +1,1 @@
+# this is md test page
